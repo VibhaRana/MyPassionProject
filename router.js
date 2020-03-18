477af1e898b61073b64829250c91764ac7497378
Vibha Rana <ranavibha02@gmail.com>
2020-03-18 00:10:30 -0700

    postController code added

--- router.js
+++ router.js
@@ -1,10 +1,16 @@
  const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
-
+const postController = require('./controllers/postController')
+//user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
+//post related routes
+//only users who have a account can create a post, no guest user can. You should register first
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+
 module.exports = router
+//color for body background #ff4c68
