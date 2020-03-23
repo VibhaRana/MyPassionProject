3e0762606319a85ba9f44a982fa18b0fd90a0a44
Vibha Rana <ranavibha02@gmail.com>
2020-03-23 16:27:21 -0700

    live socket.io

--- app.js
+++ app.js
@@ -2,49 +2,42 @@ const express = require('express')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
-const markdown = require("marked")
+const markdown = require('marked')
 const app = express()
 const sanitizeHTML = require('sanitize-html')
 
-//session package
 let sessionOptions = session({
-    secret: "Javascript is cool",
-    store: new MongoStore({client: require('./db')}),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
-
+  secret: "JavaScript is sooooooooo coool",
+  store: new MongoStore({client: require('./db')}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
 })
-//tell express to use session
+
 app.use(sessionOptions)
-//use flash
 app.use(flash())
 
-//we are telling express to use this function at every request.
-    //before we are using this just before router, means this function will run first
-app.use(function(req, res, next){
-    //make our markdown function available from within ejs template
-    res.locals.filterUserHTML = function(content){
-        return sanitizeHTML(markdown(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAttributes: {}})
-    }
-    //make all error and success flash messages available from all templates
-    res.locals.errors = req.flash("errors")
-    res.locals.success = req.flash("success")
-    //make current user id available on the req object
-    if(req.session.user){
-        req.visitorId = req.session.user._id
-    }else{
-        req.visitorId = 0
-    }
-    //we are now working with an object that will be available within ejs. thats y use locals. 
-    //make user session data available from within view templates
-    res.locals.user = req.session.user
-    //we are calling next, so express will move on to run actual function for particular route
-    next()
+app.use(function(req, res, next) {
+  // make our markdown function available from within ejs templates
+  res.locals.filterUserHTML = function(content) {
+    return sanitizeHTML(markdown(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAttributes: {}})
+  }
+  
+  // make all error and success flash messages available from all templates
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
+
+  // make current user id available on the req object
+  if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+  
+  // make user session data available from within view templates
+  res.locals.user = req.session.user
+  next()
 })
+
 const router = require('./router')
 
-app.use(express.urlencoded({extended: false})) //boiler plate code, no need to memorise
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use(express.static('public'))
@@ -53,4 +46,23 @@ app.set('view engine', 'ejs')
 
 app.use('/', router)
 
-module.exports = app
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.use(function(socket, next) {
+  sessionOptions(socket.request, socket.request.res, next)
+})
+
+io.on('connection', function(socket) {
+  if (socket.request.session.user) {
+    let user = socket.request.session.user
+
+    socket.emit('welcome', {username: user.username, avatar: user.avatar})
+
+    socket.on('chatMessageFromBrowser', function(data) {
+      socket.broadcast.emit('chatMessageFromServer', {message: sanitizeHTML(data.message, {allowedTags: [], allowedAttributes: {}}), username: user.username, avatar: user.avatar})
+    })
+  }
+})
+
+module.exports = server
